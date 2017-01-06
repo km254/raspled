87d259c8619e0dffba119f8fffacc9d85f743eb1
yabuki kazuma <kattun1965121@gmail.com>
2017-01-06 15:42:16 +0900

    Update flash.bash

--- flash.bash
+++ flash.bash
@@ -1,1 +1,14 @@
+#! /bin/bash
 
+for((i = 0; i< 15; i++)); do
+  echo 1 > /dev/myled0
+  sleep 1
+  echo 0 > /dev/myled0
+  sleep 0.5
+  echo 1 > /dev/myled0
+  sleep 0.5
+  echo 0 > /dev/myled0
+  sleep 1
+  
+done
+  
